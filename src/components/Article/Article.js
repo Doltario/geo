c1457482c445281e324f9f659c186c06714ded28
Félix Darraud <felix.darraud@gmail.com>
2020-06-24 09:22:58 +0200

    Added progress bar, light menu, gsap animation, scroll to content button + Add style

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,5 +1,10 @@
 import Navigation from '@components/Navigation/Navigation.vue';
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
   components: {
     Navigation,
@@ -8,29 +13,59 @@ export default {
     return {
       sliderWidth: 0,
       sliderTranslation: 0,
+      progressBarWidth: 0,
+      lightMenu: false,
       article: {
         tags: ['Environnement', 'Protection'],
         title: 'Les tortues marines mangent du plastique car elles sont attirées par son odeur',
         author: 'AFP',
         publicationDate: 1592402940,
         bgUrl: 'http://localhost:8080/images/articles/bg-article-2.png',
+        content: {
+          image1: 'http://localhost:8080/images/articles/article-content-2-1.png',
+          title1: 'Peuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia id deserunt mollit anim id est laborum.',
+          image2: 'http://localhost:8080/images/articles/article-content-2-2.png',
+          image3: 'http://localhost:8080/images/articles/article-content-2-3.png',
+        },
       },
     };
   },
   mounted: function() {
     this.computeSliderWidth();
 
-    document.addEventListener('wheel', this.computeTranslation);
+    document.addEventListener('wheel', this.scrollListener);
+
+    gsap.from('.image-left', {
+      scrollTrigger: '.image-left',
+      y: -500,
+      duration: 5,
+      horizontal: true,
+    });
   },
   methods: {
+    scrollToContent: function() {
+      window.clearInterval();
+
+      const interval = setInterval(() => {
+        if (this.sliderTranslation > -window.innerWidth) {
+          this.sliderTranslation = this.sliderTranslation - 5 * 5;
+          this.lightMenu = true;
+          this.computeProgressBarWidth();
+        } else {
+          window.clearInterval(interval);
+        }
+      }, 10);
+    },
     computeSliderWidth: function() {
       this.sliderWidth = 0;
       const sliderItems = document.querySelectorAll('.slider-content > *');
+
       sliderItems.forEach((sliderItem) => {
-        this.sliderWidth += sliderItem.getBoundingClientRect().width;
+        this.sliderWidth += Math.ceil(sliderItem.getBoundingClientRect().width);
       });
     },
-    computeTranslation: function(event) {
+    scrollListener: function(event) {
+      // Compute Translations
       let newSliderTranslation = this.sliderTranslation - event.deltaY;
 
       if (newSliderTranslation && newSliderTranslation > 0) {
@@ -43,6 +78,22 @@ export default {
         }
         this.sliderTranslation = newSliderTranslation;
       }
+
+      // Trigger dark/light menu on scroll
+      // if (this.sliderTranslation * -1 > document.querySelector('.slider-content').firstElementChild.getBoundingClientRect().width) {
+      if (this.sliderTranslation < 0) {
+        this.lightMenu = true;
+      } else {
+        this.lightMenu = false;
+      }
+
+      //Progress bar width
+      this.computeProgressBarWidth();
+    },
+    computeProgressBarWidth: function() {
+      const progressBarWidth = (window.innerWidth / (this.sliderWidth - window.innerWidth)) * this.sliderTranslation;
+
+      this.progressBarWidth = progressBarWidth * -1;
     },
   },
 };
